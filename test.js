9c1e7fb774d80a4d8dcfa5b72b054d28bafb8d71
halex22 <halex2209@gmail.com>
2025-03-07 13:31:31 +0100

    completed all methods

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-export const rgbToHsl = (r, g, b) => {
+ const rgbToHsl = (r, g, b) => {
   r /= 255;
   g /= 255;
   b /= 255;
@@ -12,11 +12,23 @@ export const rgbToHsl = (r, g, b) => {
       : 4 + (r - g) / s
     : 0;
   return [
-    60 * h < 0 ? 60 * h + 360 : 60 * h,
-    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
-    (100 * (2 * l - s)) / 2,
+    Math.round(60 * h < 0 ? 60 * h + 360 : 60 * h),
+    Math.round(100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)),
+    Math.round((100 * (2 * l - s)) / 2),
   ];
 };
+
+const hslToRgb = (h, s, l) => {
+  s /= 100;
+  l /= 100;
+  const k = n => (n + h / 30) % 12;
+  const a = s * Math.min(l, 1 - l);
+  const f = n =>
+    l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+  return [Math.round(255 * f(0)), Math.round(255 * f(8)), Math.round(255 * f(4))];
+};
+
+
 
 // for (let index = 0; index < 1000; index++) {
 //   const alpha = randomAlpha()
@@ -25,4 +37,6 @@ export const rgbToHsl = (r, g, b) => {
 // }
 
 
-console.log(rgbToHsl(255,61,12))
+console.log(rgbToHsl(12, 255, 89))
+
+console.log(hslToRgb(139.01234567901236, 100, 52.352941176470594 ))
